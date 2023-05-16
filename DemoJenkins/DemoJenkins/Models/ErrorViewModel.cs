49ac5e6555eb5f660daab5dd0e0d4e73dfd39bd5
subinoy-iiita <subinoy710@gmail.com>
2023-05-16 23:51:53 +0530

    Adding demo webapp

---
+++ DemoJenkins/DemoJenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoJenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
